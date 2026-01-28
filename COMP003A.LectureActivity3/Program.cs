de5982555bd6c79b6c04338f3ab5859175a9f727
samian <sam.iannazzo@yahoo.com>
2026-01-27 23:21:58 -0800

    added else if to line 15, console.writeline to line17

--- COMP003A.LectureActivity3/Program.cs
+++ COMP003A.LectureActivity3/Program.cs
@@ -9,9 +9,15 @@
             int currentMonth = DateTime.Now.Month;
 
             if (currentMonth >= 1 && currentMonth <= 5)
+            { 
                 Console.WriteLine("Spring Term");
+            }
+            else if (currentMonth >=6 && currentMonth <=7)
+            {
+                Console.WriteLine("Summer Term");
+            }
 
-            Console.WriteLine("SECTION B - Campus Access Check");
+                Console.WriteLine("SECTION B - Campus Access Check");
 
             Console.WriteLine("SECTION C - Semester Code Loopup");
         }
